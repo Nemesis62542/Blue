44f9dd9fe49e2e29b399e6b46bb7def0808c4bb0
Nemesis62542 <noboru.tyrannosaurus@gmail.com>
2025-05-20 17:39:14 +0900

    ゲージの影も同時に減らすように処理を追加

--- Assets/Scripts/UI/PlayerStatusView.cs
+++ Assets/Scripts/UI/PlayerStatusView.cs
@@ -7,15 +7,19 @@ namespace Blue.UI
     {
         [SerializeField] private Slider hpSlider;
         [SerializeField] private Slider oxygenSlider;
+        [SerializeField] private Slider hpSliderShadow;
+        [SerializeField] private Slider oxygenSliderShadow;
 
         public void SetHPRatio(float ratio)
         {
             hpSlider.value = Mathf.Clamp01(ratio);
+            hpSliderShadow.value = hpSlider.value;
         }
 
         public void SetOxygenRatio(float ratio)
         {
             oxygenSlider.value = Mathf.Clamp01(ratio);
+            oxygenSliderShadow.value = oxygenSlider.value;
         }
     }
 }
