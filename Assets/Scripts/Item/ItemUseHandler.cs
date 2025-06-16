f34aa62f70253e0de42477a76adfb6c9d3e691e7
BuildTools <unconfigured@null.spigotmc.org>
2025-06-16 11:59:23 +0900

    アイテム使用可能回数の追加

--- Assets/Scripts/Item/ItemUseHandler.cs
+++ Assets/Scripts/Item/ItemUseHandler.cs
@@ -6,11 +6,39 @@ namespace Blue.Item
     {
         protected ItemData itemData;
 
+        [SerializeField] private bool destroyAfterUse = true;
+        [SerializeField] private int useCountRemaining = 1; // -1 = 無限使用, 0 = 即破棄
+
         public virtual void Initialize(ItemData item_data)
         {
             itemData = item_data;
         }
 
-        public abstract void Use();
+        public void Execute()
+        {
+            Use();
+
+            if (!destroyAfterUse) return;
+
+            if (useCountRemaining > 0)
+            {
+                useCountRemaining--;
+            }
+
+            if (useCountRemaining == 0)
+            {
+                OnUseLimitReached();
+            }
+        }
+
+        protected abstract void Use();
+
+        /// <summary>
+        /// 使用回数がなくなった時(useCountRemining=0)のときの処理
+        /// </summary>
+        protected virtual void OnUseLimitReached()
+        {
+            Destroy(gameObject);
+        }
     }
 }
