d86c558cde3c5edf72a2162e1f5f8982b44cf8f6
Nemesis62542 <noboru.tyrannosaurus@gmail.com>
2025-07-08 06:17:15 +0900

    ゴッドレイの表示のために調整

--- Assets/Scripts/Field/FieldController.cs
+++ Assets/Scripts/Field/FieldController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class FieldController : MonoBehaviour
 {
-    [SerializeField] GameObject waterSurface;
-    [SerializeField] ParticleSystem godRay;
-    [SerializeField] PlayerController player;
+    [SerializeField] private GameObject waterSurface;
+    [SerializeField] private ParticleSystem godRay;
+    [SerializeField] private PlayerController player;
+
+    const float GodRayThreshold = 15.0f;
 
     public float WaterLevel => waterSurface.transform.position.y;
 
@@ -17,5 +19,16 @@ public class FieldController : MonoBehaviour
     void Update()
     {
         godRay.transform.position = new Vector3(player.transform.position.x, WaterLevel, player.transform.position.z);
+
+        bool shouldPlay = WaterLevel - player.transform.position.y <= GodRayThreshold;
+
+        if (shouldPlay && !godRay.isPlaying)
+        {
+            godRay.Play();
+        }
+        else if (!shouldPlay && godRay.isPlaying)
+        {
+            godRay.Stop();
+        }
     }
 }
