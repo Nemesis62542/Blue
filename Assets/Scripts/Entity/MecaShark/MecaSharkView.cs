c87cc0fe36d8f413cea6cd494d39fd1c79173721
Nemesis62542 <noboru.tyrannosaurus@gmail.com>
2025-07-20 04:38:56 +0900

    メカシャークのロジック追加

--- Assets/Scripts/Entity/MecaShark/MecaSharkView.cs
+++ Assets/Scripts/Entity/MecaShark/MecaSharkView.cs
@@ -12,5 +12,10 @@ namespace Blue.Entity
         public SkinnedMeshRenderer Renderer => skinnedMeshRenderer;
         public void EnableHighlight() => highlightController.EnableHighlight();
         public void DisableHighlight() => highlightController.DisableHighlight();
+
+        public void LockOn(bool is_lock_on)
+        {
+            animator.SetBool("LockOn", is_lock_on);
+        }
     }
 }
