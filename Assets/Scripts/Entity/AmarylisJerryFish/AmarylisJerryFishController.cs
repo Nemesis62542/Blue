db9c5a332f92078211ed735376cb994082a813b3
Nemesis62542 <noboru.tyrannosaurus@gmail.com>
2025-07-29 03:50:44 +0900

    クラゲのData追加

---
+++ Assets/Scripts/Entity/AmarylisJerryFish/AmarylisJerryFishController.cs
@@ -0,0 +1,33 @@
+using Blue.Interface;
+using Blue.UI.Common;
+using UnityEngine;
+
+namespace Blue.Entity
+{
+    public class AmarylisJerryFishController : BaseEntityController<AmarylisJerryFishModel, AmarylisJerryFishView>, IScannable, ICapturable
+    {
+        public Renderer[] TargetRenderers => new Renderer[] { view.Renderer };
+        public Status Status => model.Status;
+        public ScanData ScanData => new ScanData(model.Status.Name, ScanData.Threat.Safety);
+        public EntityData EntityData => model.Data;
+
+        protected override void Awake()
+        {
+            model = new AmarylisJerryFishModel(data);
+        }
+
+        private void Update()
+        {
+        }
+        
+        public void OnScanEnd()
+        {
+            view.DisableHighlight();
+        }
+
+        public void OnScanStart()
+        {
+            view.EnableHighlight();
+        }
+    }
+}
