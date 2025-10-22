d57987365475f40887c21725e7f7a465423ac831
BuildTools <unconfigured@null.spigotmc.org>
2025-10-22 17:05:00 +0900

    namespace・必要なクラスの生成

---
+++ Assets/Scripts/UI/Garage/CraftTable/CraftTableController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Blue.UI.Garage.CraftTable
+{
+    public class CraftTableController : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/Garage/CraftTable/CraftTableModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Blue.UI.Garage.CraftTable
+{
+    public class CraftTableModel : MonoBehaviour
+    {
+    }
+}
