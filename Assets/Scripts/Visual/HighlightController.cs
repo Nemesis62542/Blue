342ddba019bfb7f1732a0f9f8e79dceac2d2096b
Nemesis62542 <noboru.tyrannosaurus@gmail.com>
2026-03-29 23:11:07 +0900

    ハイライト機能の改善 ・オブジェクトに遮られないように変更 ・ハイライト用マテリアルを専用のものに置き換え

--- Assets/Scripts/Visual/HighlightController.cs
+++ Assets/Scripts/Visual/HighlightController.cs
@@ -1,16 +1,31 @@
+using Blue.UI.Common;
 using UnityEngine;
 
 namespace Blue.Visual
 {
     public class HighlightController : MonoBehaviour
     {
+        private static readonly int FresnelColorId = Shader.PropertyToID("_BaseEmissionColor");
+
         [SerializeField] private Renderer targetRenderer;
         [SerializeField] private Material highlightMaterial;
 
-        private Material[] highlightedMaterials;
-        private bool isHighlighted = false;
+        [Header("Threat Colors (HDR)")]
+        [ColorUsage(true, true)]
+        [SerializeField] private Color safetyColor = new Color(0f, 2f, 0.5f, 1f);
+        [ColorUsage(true, true)]
+        [SerializeField] private Color warningColor = new Color(2f, 1.5f, 0f, 1f);
+        [ColorUsage(true, true)]
+        [SerializeField] private Color dangerColor = new Color(2f, 0f, 0f, 1f);
+        [ColorUsage(true, true)]
+        [SerializeField] private Color defaultColor = new Color(0f, 1f, 2f, 1f);
 
+        private Material[] highlightedMaterials;
+        private MaterialPropertyBlock propertyBlock;
+
+        // MecaSharkView等で死亡時にマテリアル差し替えのため公開
         public Material[] baseMaterials;
+        private bool isHighlighted = false;
 
         private void Awake()
         {
@@ -19,6 +34,8 @@ namespace Blue.Visual
             highlightedMaterials = new Material[baseMaterials.Length + 1];
             baseMaterials.CopyTo(highlightedMaterials, 0);
             highlightedMaterials[baseMaterials.Length] = highlightMaterial;
+
+            propertyBlock = new MaterialPropertyBlock();
         }
 
         public void EnableHighlight()
@@ -29,12 +46,39 @@ namespace Blue.Visual
             isHighlighted = true;
         }
 
+        public void EnableHighlight(ScanData.Threat threat)
+        {
+            if (!isHighlighted)
+            {
+                targetRenderer.sharedMaterials = highlightedMaterials;
+                isHighlighted = true;
+            }
+
+            Color color = GetColorForThreat(threat);
+            int highlightMaterialIndex = highlightedMaterials.Length - 1;
+
+            targetRenderer.GetPropertyBlock(propertyBlock, highlightMaterialIndex);
+            propertyBlock.SetColor(FresnelColorId, color);
+            targetRenderer.SetPropertyBlock(propertyBlock, highlightMaterialIndex);
+        }
+
         public void DisableHighlight()
         {
             if (!isHighlighted) return;
 
             targetRenderer.sharedMaterials = baseMaterials;
             isHighlighted = false;
+        }
+
+        private Color GetColorForThreat(ScanData.Threat threat)
+        {
+            return threat switch
+            {
+                ScanData.Threat.Safety => safetyColor,
+                ScanData.Threat.Warning => warningColor,
+                ScanData.Threat.Danger => dangerColor,
+                _ => defaultColor
+            };
         }
     }
 }
